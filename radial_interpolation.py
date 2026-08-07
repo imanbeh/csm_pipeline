bcaddffed8b994b50f12c32d2ca3002cb10fc04a
Iman Behbehani <85190270+imanbeh@users.noreply.github.com>
2026-08-07 15:27:26 -0400

    updated radial_interpolation script with new changes

--- radial_interpolation.py
+++ radial_interpolation.py
@@ -8,24 +8,29 @@ from matplotlib import pyplot as plt
 from astropy import units as u
 from scipy.interpolate import RectBivariateSpline
 
+def radial_interp(image, cx,cy, n_lines, n_pts, plot=True):
+    '''
+    runs radial interpolation sequence
+    plots resulting comparison
 
+    returns:
+    Plots comparison of original image and interpolated image
+    x_vals: x value grid where each row corresponds to the x values along a given ray
+    y_vals: y value grid where ... y values along a given ray
+    interp_star: grad where rows correspond to interpolated values along a given ray
+    '''
+    x_vals,y_vals,interp_star = ray_calculation(image, cx,cy, n_lines, n_pts, plot=True)
+    return x_vals,y_vals,interp_star
 
-def make_spline(image):
+def make_spline(x,y,image):
     '''
     makes RectBivariateSpline for your 2d dataset 
     returns spline
     '''
-    # get x and y grid
-    x = np.linspace(0,image[0,:].shape[0]-1,image[0,:].shape[0])
-    y = np.linspace(0,image[:,0].shape[0]-1,image[:,0].shape[0])
 
-    spline = RectBivariateSpline(y,x,image) # making function to interpolate starry array
+    spline = RectBivariateSpline(x,y,image) # making function to interpolate starry array
 
     return spline
-
-
-
-#X,Y = np.meshgrid(x,y)
 
 def ray_calculation(image, cx,cy, n_lines, n_pts, plot=True):
     '''
@@ -39,49 +44,77 @@ def ray_calculation(image, cx,cy, n_lines, n_pts, plot=True):
     plot = booleon T/F to plot resulting interpolated grid
 
     '''
+
     psis = np.linspace(0,2*np.pi, n_lines, endpoint=False)
     rs = np.linspace(0,cx+1,n_pts)
-    spline = make_spline(image) #makes interpolation spline on data
 
-    rys = np.zeros((n_lines, n_pts)) # make ray array. rows will interate through psis,
-                                    # cols iterate through radii
+    # get x and y grid
+    x = np.linspace(0,image[0,:].shape[0]-1,image[0,:].shape[0])
+    y = np.linspace(0,image[:,0].shape[0]-1,image[:,0].shape[0])
+    X,Y = np.meshgrid(x,y)
 
+    spline = make_spline(x,y,image) #makes interpolation spline on data
+
+    x_vals = np.zeros((n_lines, n_pts))
+    y_vals = np.zeros((n_lines, n_pts))
     rys_xy = np.full((n_lines,n_pts),tuple) # make emmpty tuple array for cartesian coords corresponding to interpolated star
 
     #pick coordinates for interpolation
     for i, psi in enumerate(psis):
 
         # polar to cartesian
-        
         x_r = cx + rs * np.cos(psi) #add cx and cy to x and y values to shift origin to image center instead of (0,0)
         y_r = cy + rs * np.sin(psi)
 
+        x_vals[i] = x_r
+        y_vals[i] = y_r
 
-        #interpolate a line of values
-        new_ray = spline(x_r,y_r,grid=False)
-
-        # give ray at psi = i interpolated values
-        rys[i,:] = new_ray
         # xy coordinates for each ray index 
         for j in range(n_pts):
             rys_xy[i,j]=(x_r[j],y_r[j])
+
+    # now interpolate!
+    interp_star = spline(x_vals,y_vals, grid=False)
         
     if plot==True:
-        plot_interp_grid(rys,rys_xy)
+        plot_interp_grid(X,Y,image,x_vals,y_vals,interp_star)
 
-    return rys,rys_xy
+    return x_vals,y_vals,interp_star
 
-def plot_interp_grid(rys,rys_xy):
+def plot_interp_grid(X,Y,image,x_vals,y_vals,interp_star):
 
-    # get x and y vals from coordinate grid
-    x_vals = [x[0] for row in rys_xy for x in row] # gets x values
-    y_vals = [x[1] for row in rys_xy for x in row] # gets y values
+    fig, ax = plt.subplots(nrows=1, ncols=2,figsize=(8,4))
+    ax[0].pcolormesh(X, Y, image.value)
+    ax[0].set_title("Original Image")
+    ax[1].pcolormesh(x_vals, y_vals, interp_star)
+    ax[1].set_title("Interpolated Image")
+
+    plt.show()
+
+
+def plot_rays(x_grid,y_grid,interp_star):
+    '''
+    data validation step to show polar alignment
+    '''
+    # making temp dataset to add rays of a different value
+    # this will plot as radial lines
+    interp_box_temp = np.zeros((len(interp_star),len(interp_star[0])))
+
+    # making rays for reference
+    for i in range(len(interp_star)):
+        for j in range(len(interp_star[0])):
+            #print(i)
+            if i%10==0:
+                interp_box_temp[i,j] = 100
+                #print(i,j)
+            else:
+                interp_box_temp[i,j] = interp_star[i,j]
 
     plt.figure(figsize=(6,5))
-    plt.scatter(x_vals,y_vals,c=rys)
-    # plt.xlim(0,100)
-    # plt.ylim(0,100)
-    #plt.gca().invert_yaxis()
+    plt.pcolormesh(x_grid,y_grid,interp_box_temp)
 
-    plt.title("Radially Interpolated Star")
+    plt.vlines(49,0,100,colors='red')
+    plt.hlines(49,0,100,colors='red')
+
+    plt.title("Plotting Interpolated Rays")
     plt.colorbar()
